62c915181fecff2b08c3ed1a060d4789e4dc0145
Alon Hazan <alonhaz02@gmail.com>
2024-08-12 15:23:13 +0300

    Final test

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-  //host: '0.0.0.0',   // This allows access from any local IP address
-    port: 5173,        // Ensure the server runs on port 5173
-    strictPort: true,  // Ensure the server always uses the specified port
-    hmr: true,         // Hot Module Replacement for faster development
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
   },
 });
